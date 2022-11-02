68c0dac65883b5be5c2d1689ce183bf65ede72a6
Sparrow <dfleites1234@gmail.com>
2022-11-02 13:48:17 -0500

    Types of Events and Event Handlers

--- starter/script.js
+++ starter/script.js
@@ -126,7 +126,6 @@ logo.classList.contains('c');
 // Don't use
 logo.className = 'jonas';
 
-*/
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Implementing Smooth Scrolling
@@ -162,3 +161,23 @@ btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Types of Events and Event Handlers
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+
+  h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
