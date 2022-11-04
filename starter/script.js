d50ce8c325c0999cd8afadfb44e69fd36a967a1d
Sparrow <dfleites1234@gmail.com>
2022-11-04 13:13:08 -0500

    Building a Tabbed Component

--- starter/script.js
+++ starter/script.js
@@ -238,7 +238,6 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Event Delegation: Implementing Page Navigation
 
-*/
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // DOM Traversing
@@ -270,4 +269,31 @@ console.log(h1.nextSibling);
 console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale()0.5';
+});
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Building a Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
